781656827a04c6de911fefa6350c8eaba6b0612e
damon-st <brayan14-cevallos@hotmail.com>
2024-01-01 14:46:19 -0500

    signup remove

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  // return <SignUp />;
+  return (
+    <div className="w-full h-full flex items-center justify-center">
+      <h2 className="text-3xl">Crear usuario no esta habilitado</h2>
+    </div>
+  );
 }
